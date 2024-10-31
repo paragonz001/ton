808fb65defd64170889e6d515207bd73b3983c1b
paragonz001 <paragonlegend001@gmail.com>
2024-10-31 23:22:55 +0700

    build

---
+++ dist/wallets/signing/singer.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="node" />
+/// <reference types="node" />
+import { Builder, Cell } from "@ton/core";
+export type SendArgsSigned = {
+    secretKey: Buffer;
+};
+export type SendArgsSignable = {
+    signer: (message: Cell) => Promise<Buffer>;
+};
+export declare function signPayload<T extends SendArgsSigned | SendArgsSignable>(args: T, signingMessage: Builder, packMessage: (signature: Buffer, signingMessage: Builder) => Cell): T extends SendArgsSignable ? Promise<Cell> : Cell;
